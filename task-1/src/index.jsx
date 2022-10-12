a5475817209c434aac1fd092cd42da97f68fd014
VitaliiHryb <vitaliigryb@ukr.net>
2022-10-12 19:16:42 +0300

    Context - Themed buttons

--- task-1/src/index.jsx
+++ task-1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import Counter from './Counter';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Counter />);
+root.render(<App />);
