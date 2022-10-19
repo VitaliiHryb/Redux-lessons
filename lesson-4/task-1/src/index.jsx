459a5ea317848b5d36647ff6daf2ca886919ad64
VitaliiHryb <vitaliigryb@ukr.net>
2022-10-19 14:53:24 +0300

    React Redux Counter :sunglasses:

---
+++ lesson-4/task-1/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+/*
+<!-- App -->
+<!-- Provider -->
+<!-- Counter -->
+<div class="counter">
+    <button class="counter__button">
+      -
+    </button>
+    <span class="counter__value">
+      17
+    </span>
+    <button class="counter__button">
+      +
+    </button>
+  </div>
+*/
