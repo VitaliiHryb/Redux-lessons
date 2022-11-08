443d16394e01ff2f6c3640ab32b04b6c4fbae0f0
VitaliiHryb <vitaliigryb@ukr.net>
2022-11-08 17:40:38 +0200

    store add functional

--- lesson-6/task-2/src/store.js
+++ lesson-6/task-2/src/store.js
@@ -6,26 +6,28 @@ const reducer = combineReducers({
   users: usersReducer,
 });
 
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk), applyMiddleware(logger)),
+);
 
-// const logger = store => next => action => {
-//   // console.log(action.type);
-//   console.group(action.type);
-//   console.info(action);
-//   const result = next(action);
-//   console.log('new state', store.getState());
-//   console.groupEnd();
-//   return result;
-// };
+// const reducer = combineReducers({
+//   users: usersReducer,
+// });
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const store = createStore(
-//   counterReducer,
-//   composeEnhancers(applyMiddleware(logger)),
-//   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-// );
+// const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
