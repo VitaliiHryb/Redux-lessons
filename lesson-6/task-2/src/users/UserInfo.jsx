30de9d5e8e7776f3a9b8b47030c56b98e1be4924
VitaliiHryb <vitaliigryb@ukr.net>
2022-11-08 17:47:04 +0200

    UserInfo

--- lesson-6/task-2/src/users/UserInfo.jsx
+++ lesson-6/task-2/src/users/UserInfo.jsx
@@ -2,6 +2,38 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { isFetchingSelector, userDataSelector } from './users.selectors';
 import Spinner from './Spinner';
+
+function UserInfo({ userData, isFetching }) {
+  if (isFetching) return <Spinner />;
+  if (!userData) return null;
+  return (
+    <div className="user">
+      <img
+        alt="User Avatar"
+        src={userData.avatar_url}
+        className="user__avatar"
+      />
+      <div className="user__info">
+        <span className="user__name">{userData.name}</span>
+        <span className="user__location">{`from ${userData.location}`}</span>
+      </div>
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    isFetching: isFetchingSelector(state),
+    userData: userDataSelector(state),
+  };
+}
+
+export default connect(mapStateToProps)(UserInfo);
+/*
+import React from 'react';
+import { connect } from 'react-redux';
+import { isFetchingSelector, userDataSelector } from './users.selectors';
+import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 
 const UserInfo = ({ userData, isFetching }) => {
@@ -45,3 +77,4 @@ const mapState = state => {
 };
 
 export default connect(mapState)(UserInfo);
+*/
