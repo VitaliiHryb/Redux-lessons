3c910fce51b0fa68a454e2c743e5aa43caf2d46a
VitaliiHryb <vitaliigryb@ukr.net>
2022-11-08 17:45:03 +0200

    Загруженные данные нужно отобразить в компоненте UserInfo

--- lesson-6/task-2/src/users/UserInfo.jsx
+++ lesson-6/task-2/src/users/UserInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { isFetchingSelector, userDataSelector } from './users.selector';
+import { isFetchingSelector, userDataSelector } from './users.selectors';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 
